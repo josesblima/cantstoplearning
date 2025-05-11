71bef7644da91e5e1a71ae55fa2684b4d7c7dfae
josesblima <josesblima@gmail.com>
2025-05-11 23:05:17 +0100

    initial sketch before implementing ui framework

---
+++ src/pages/App.jsx
@@ -0,0 +1,71 @@
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div class="container">
+      <div class="title">
+        <div class="left">
+          <h3>Can't Stop Learning, Help!</h3>
+          <h5>(a simple blog on interesting programming topics)</h5>
+        </div>
+        <div class="right">
+          <h3>Join the Newsletter!</h3>
+        </div>
+      </div>
+      <div class="bigTitle">
+        <div class="left">
+          <h1>H1 Lorem Ipsum</h1>
+          <h2>H2 Lorem Ipsum</h2>
+          <h3>H3 Lorem Ipsum</h3>
+          <h4>H4 Lorem Ipsum</h4>
+        </div>
+        <div class="right">
+          <img src="neovim.png"></img>
+        </div>
+      </div>
+      <div class="triSection">
+        <div class="left">
+          <h2>This is a Title</h2>
+          <h4>
+            This is a lot of text below the title, it describes everything, in a
+            succint manner, monkey banana
+          </h4>
+          <h3>This is aanother Title but smaller</h3>
+          <h4>
+            This is even more text, to make it seem as if there's actual
+            relevant information on this blog
+          </h4>
+        </div>
+        <div class="middle">
+          <img src="vertical_image.png"></img>
+        </div>
+        <div class="right">
+          <div class="topRight">
+            <div class="one">
+              <h2>Title</h2>
+              <h3>Text and all</h3>
+            </div>
+            <div class="two">
+              <h2>Title</h2>
+              <h3>Text and all</h3>
+            </div>
+            <div class="three">
+              <h2>Title</h2>
+              <h3>Text and all</h3>
+            </div>
+          </div>
+          <div class="four">
+            <h2>Title</h2>
+            <h3>Text and all</h3>
+          </div>
+          <div class="five">
+            <h2>Title</h2>
+            <h3>Text and all</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
